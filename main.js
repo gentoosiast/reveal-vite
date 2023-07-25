d2cf77fc980288c91b0620af1276b28b5255d072
Sergey Koksharov <gentoosiastic@gmail.com>
2023-07-25 12:43:23 +0300

    fix: add Prettier & EditorConfig configs

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import Reveal from "reveal.js";
-import RevealNotes from "reveal.js/plugin/notes/notes";
-import RevealZoom from "reveal.js/plugin/zoom/zoom";
-import "reveal.js/dist/reveal.css";
+import Reveal from 'reveal.js';
+import RevealNotes from 'reveal.js/plugin/notes/notes';
+import RevealZoom from 'reveal.js/plugin/zoom/zoom';
+import 'reveal.js/dist/reveal.css';
 // see available themes in the
 // node_modules/reveal.js/dist/theme
 //  beige, black, blood, league, moon, night, serif, simple, ...
 // import ComparisonSlider from "comparison-slider";
-import "reveal.js/dist/theme/moon.css";
+import 'reveal.js/dist/theme/moon.css';
 
 const deck = new Reveal();
 deck.initialize({
